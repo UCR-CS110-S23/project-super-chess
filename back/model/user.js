ad691efc92505b1fc34b9f6b4b5e0e95809d0310
CalebMCode <caleb.n.mcclure@gmail.com>
2023-06-11 15:07:36 -0700

    Added hashing to project

--- back/model/user.js
+++ back/model/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -6,19 +8,41 @@ const userSchema = new mongoose.Schema({
         unique: true,
         type: String
     },
-    password: {
-        required: true,
-        type: String
-    },
     name: {
         type: String,
         required: true,
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    hash: {
+        type: String,
+        required: true
     },
     rooms: {
         type: Array,
         required: true,
     }
 
-})
 
-module.exports = mongoose.model('User', userSchema)
+});
+
+
+userSchema.pre("save",   function (){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    console.log(this.password)
+    this.hash = crypto.pbkdf2Sync(this.hash, this.salt,
+        1000, 64, `sha512`).toString(`hex`);
+});
+
+
+
+const validPassword = async function (password) {
+    let hash = crypto.pbkdf2Sync(password,
+        this.salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.hash === hash;
+};
+
+
+module.exports = mongoose.model('User', userSchema);
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const User = require('../model/user');
+const crypto = require("crypto");
+
 const router = express.Router()
 
 module.exports = router;
@@ -9,39 +11,39 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     // check if user in database
-    const user = await User.findOne({ username });
-    
-    if (!user)
-      return res.json({ msg: "Incorrect Username ", status: false });
-    else if (user.password !== password)
-      return res.json({ msg: "Incorrect Password", status: false });
-    else {
-      session.authenticated = true;
-      session.username = username;
-      res.json({ msg: "Logged in", username: username, status: true });
+    const user = await User.findOne({ username: username }).exec();
+
+    console.log(user);
+    let currentHash = crypto.pbkdf2Sync(password,
+        user.salt, 1000, 64, `sha512`).toString(`hex`);
+    if(currentHash === user.hash){
+        session.authenticated = true;
+        session.username = username;
+        res.json({ msg: "Logged in", username: username, status: true });
+    }else {
+        return res.json({ msg: "Incorrect Password", status: false });
     }
+
 });
 
 // DONE: Add login functionality
 router.post('/signup',  async (req, res)=>{
-  const {username, password, name} = req.body;
-  const user = new User ({
-      username: username,
-      password: password,
-      name: name
-
-  })
-
-  try{
-      const dataSaved = await user.save();
-      res.status(200).json(dataSaved);
-  }
-  catch (error){
-      console.log(error);
-      res.send("ERROR!");
-  }
+    const { username, password } = req.body;
+    let user = new User({
+        name: req.body.name,
+        username: req.body.username,
+        hash: req.body.password,
+        salt: "salt",
+    });
+    try{
+        const dataSaved = await user.save();
+        res.status(200).json(dataSaved);
+    }
+    catch (error){
+        console.log(error);
+        res.send("ERROR!");
+    }
 })
-
 //Add change option
 
 // Set up a route for the logout page
