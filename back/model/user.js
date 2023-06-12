6cb0f7899c92fbdb21cb6907a501d8b96026f057
2314577483 <liuzhenjie0619@gmail.com>
2023-06-12 04:50:53 -0700

    fixed errors

--- back/model/user.js
+++ back/model/user.js
@@ -1,48 +1,43 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-
+const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        required: true,
-        unique: true,
-        type: String
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    salt: {
-        type: String,
-        required: true
-    },
-    hash: {
-        type: String,
-        required: true
-    },
-    rooms: {
-        type: Array,
-        required: true,
-    }
-
-
+  username: {
+    required: true,
+    unique: true,
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+    required: true,
+  },
+  hash: {
+    type: String,
+    required: true,
+  },
+  rooms: {
+    type: Array,
+    required: true,
+  },
 });
 
-
-userSchema.pre("save",   function (){
-    this.salt = crypto.randomBytes(16).toString('hex');
-    console.log(this.password)
-    this.hash = crypto.pbkdf2Sync(this.hash, this.salt,
-        1000, 64, `sha512`).toString(`hex`);
+userSchema.pre("save", function () {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  console.log(this.password);
+  this.hash = crypto
+    .pbkdf2Sync(this.hash, this.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
 });
-
-
 
 const validPassword = async function (password) {
-    let hash = crypto.pbkdf2Sync(password,
-        this.salt, 1000, 64, `sha512`).toString(`hex`);
-    return this.hash === hash;
+  let hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  return this.hash === hash;
 };
 
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,54 +1,53 @@
-const express = require('express');
-const User = require('../model/user');
+const express = require("express");
+const User = require("../model/user");
 const crypto = require("crypto");
 
-const router = express.Router()
+const router = express.Router();
 
 module.exports = router;
 
-router.post('/login', async (req, res) => {
-    const { session } = req;
-    const { username, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { session } = req;
+  const { username, password } = req.body;
 
-    // check if user in database
-    const user = await User.findOne({ username: username }).exec();
+  // check if user in database
+  const user = await User.findOne({ username: username }).exec();
 
-    console.log(user);
-    let currentHash = crypto.pbkdf2Sync(password,
-        user.salt, 1000, 64, `sha512`).toString(`hex`);
-    if(currentHash === user.hash){
-        session.authenticated = true;
-        session.username = username;
-        res.json({ msg: "Logged in", username: username, status: true });
-    }else {
-        return res.json({ msg: "Incorrect Password", status: false });
-    }
-
+  console.log(user);
+  let currentHash = crypto
+    .pbkdf2Sync(password, user.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  if (currentHash === user.hash) {
+    session.authenticated = true;
+    session.username = username;
+    res.json({ msg: "Logged in", username: username, status: true });
+  } else {
+    return res.json({ msg: "Incorrect Password", status: false });
+  }
 });
 
 // DONE: Add login functionality
-router.post('/signup',  async (req, res)=>{
-    const { username, password } = req.body;
-    let user = new User({
-        name: req.body.name,
-        username: req.body.username,
-        hash: req.body.password,
-        salt: "salt",
-    });
-    try{
-        const dataSaved = await user.save();
-        res.status(200).json(dataSaved);
-    }
-    catch (error){
-        console.log(error);
-        res.send("ERROR!");
-    }
-})
+router.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  let user = new User({
+    name: req.body.name,
+    username: req.body.username,
+    hash: req.body.password,
+    salt: "salt",
+  });
+  try {
+    const dataSaved = await user.save();
+    res.status(200).json(dataSaved);
+  } catch (error) {
+    console.log(error);
+    res.send("ERROR!");
+  }
+});
 //Add change option
 
 // Set up a route for the logout page
-router.get('/logout', (req, res) => {
-    // Clear the session data and redirect to the home page
-    req.session.destroy();
-    res.send({msg: "Logged out", status: true})
-  });
+router.get("/logout", (req, res) => {
+  // Clear the session data and redirect to the home page
+  req.session.destroy();
+  res.send({ msg: "Logged out", status: true });
+});
